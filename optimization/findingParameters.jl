76521f83573fd363bb29f84425818dfb53bf248b
aurorarossi <aurora98rossi@gmail.com>
2023-03-01 13:55:05 +0100

    Add load hyperbolic data

--- optimization/findingParameters.jl
+++ optimization/findingParameters.jl
@@ -1,8 +1,5 @@
 using NPZ, Random, Distributions, LinearAlgebra, Permutations, Plots, JLD2, Statistics, DelimitedFiles, Dierckx
 
-thresholds = append!(collect(0.2:0.05:0.90), collect(0.92:0.02:0.98))
-subjects = readdlm("TemporalBrainNetworksCode/src/filtered-subjects-mod.txt", Int)
-
 # LOAD DATA 
 function load_fMRI_data(subjects, thresholds)
     # initialize variables
@@ -47,8 +44,44 @@ function compute_quantiles(average_deg_all, clustering_all, path_len_all, temp_c
     return average_deg_quantile, clustering_quantile, path_len_quantile, small_world_quantile, small_world_SB_quantile, temp_clustering_quantile
 end
 
+function load_hyperbolic_data(len_R=length(Rrange))
+    αrange = collect(0.5:0.025:1.2)
+    velocities = collect(0.1:0.1:0.9)
+    len_vel = length(velocities)
+    len_α = length(αrange)
+    average_deg_HY = zeros(len_vel, len_R, len_α)
+    clustering_HY = zeros(len_vel, len_R, len_α)
+    path_len_HY = zeros(len_vel, len_R, len_α)
+    temp_clustering_HY = zeros(len_vel, len_R, len_α)
+    small_world_HY = zeros(len_vel, len_R, len_α)
+    small_world_SB_HY = zeros(len_vel, len_R, len_α)
+    for a in 1:len_α
+        @load "/user/aurossi/home/mri_networks/hyperbolicbrains/optimization/hyperbolic_300_alpha_$(round(Int,αrange[a]*100))_R_05_05_14_v_01_01_09_correct.jld2" avHY clHY plHY tccHY
+        average_deg_HY[:, :, a] = avHY[:, :]
+        clustering_HY[:, :, a] = clHY[:, :]
+        path_len_HY[:, :, a] = plHY[:, :]
+        temp_clustering_HY[:, :, a] = tccHY[:, :]
+        small_world_HY[:, :, a] = clHY[:, :] ./ plHY[:, :]
+        small_world_SB_HY[:, :, a] = tccHY[:, :] ./ plHY[:, :]
+    end
+    return average_deg_HY, clustering_HY, path_len_HY, temp_clustering_HY, small_world_HY, small_world_SB_HY
+end
+
+function compute_integral(variable_fMRI, variable_HY, len_vel=length(velocities), len_α=length(αrange))
+    integral = zeros(len_vel, len_α)
+
+
+end
+
+
 function main()
+    thresholds = append!(collect(0.2:0.05:0.90), collect(0.92:0.02:0.98))
+    subjects = readdlm("TemporalBrainNetworksCode/src/filtered-subjects-mod.txt", Int)
+    Rrange=collect(0.5:0.5:14)
     average_deg_all, clustering_all, path_len_all, temp_clustering_all = load_fMRI_data(subjects, thresholds)
     average_deg_quantile, clustering_quantile, path_len_quantile, small_world_quantile, small_world_SB_quantile, temp_clustering_quantile = compute_quantiles(average_deg_all, clustering_all, path_len_all, temp_clustering_all, thresholds)
-    p=plot(average_deg_quantile,small_world_quantile[2,:],label="Real data 1050 Schaefer",lw=3)
+    average_deg_HY, clustering_HY, path_len_HY, temp_clustering_HY, small_world_HY, small_world_SB_HY = load_hyperbolic_data()
+
+    p = plot(average_deg_quantile, small_world_quantile[2, :], label="Real data 1050 Schaefer", lw=3)
+    plot!(average_deg_HY[1,:,1], small_world_HY[1, :,1], label="", lw=3, ls=:dash)
 end
