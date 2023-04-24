15cfdd65a370a4b51423dcd2b890b51a2d13f488
aurorarossi <aurora98rossi@gmail.com>
2023-04-24 11:37:18 +0200

    Add experiments WS

--- experiments_SW/WattsStrogatz.jl
+++ experiments_SW/WattsStrogatz.jl
@@ -13,8 +13,78 @@ struct WattsStrogatz <: TemporalNetwork
     function WattsStrogatz(number_nodes::Int64, number_snapshots::Int64, degree::Int64, β::Float64)
         adjacency=zeros(number_nodes,number_nodes,number_snapshots)
         for t in 1:number_snapshots
-            adjacency[:, :, t] .= Matrix(adjacency_matrix(watts_strogatz(number_nodes, degree, β)))
+            adjacency[:, :, t] .= Matrix(Graphs.adjacency_matrix(Graphs.watts_strogatz(number_nodes, degree, β)))
         end
         new(adjacency, number_nodes, number_snapshots, degree, β)
+    end
+end
+
+
+function adj_watts_strogatz_temporal(
+    n::Integer,
+    k::Integer,
+    β::Real;
+    is_directed::Bool=false,
+    remove_edges::Bool=true,
+)
+    @assert k < n
+    adj = zeros(n, n, div(k, 2)*n+1)
+    # If we have n - 1 neighbors (exactly k/2 on each side), then the graph is
+    # necessarily complete. No need to run the Watts-Strogatz procedure:
+    if k == n - 1 && iseven(k)
+        return is_directed ? complete_digraph(n) : complete_graph(n)
+    end
+
+    g = is_directed ? SimpleDiGraph(n) : SimpleGraph(n)
+
+    # The ith next vertex, in clockwise order.
+    # (Reduce to zero-based indexing, so the modulo works, by subtracting 1
+    # before and adding 1 after.)
+    @inline target(s, i) = ((s + i - 1) % n) + 1
+
+    # Phase 1: For each step size i, add an edge from each vertex s to the ith
+    # next vertex, in clockwise order.
+    t=2
+    for i in 1:div(k, 2), s in 1:n
+        add_edge!(g, s, target(s, i))
+    end
+    adj[:,:,1] .= Matrix(Graphs.adjacency_matrix(g))
+    for i in 1:div(k, 2), s in 1:n
+
+        # We only rewire with a probability β, and we only worry about rewiring
+        # if there is some vertex not connected to s; otherwise, the only valid
+        # rewiring is to reconnect to the ith next vertex, and there is no work
+        # to do.
+        (rand() < β && degree(g, s) < n - 1) || continue
+
+        t = target(s, i)
+
+        while true
+            d = rand(1:n)          # Tentative new target
+            d == s && continue          # Self-loops prohibited
+            d == t && break             # Rewired to original target
+            if add_edge!(g, s, d)       # Was this valid (i.e., unconnected)?
+                remove_edges && rem_edge!(g, s, t)     # True rewiring: Delete original edge
+                break                                   # We found a valid target
+            end
+        end
+        adj[:,:,t] .= Matrix(Graphs.adjacency_matrix(g))
+        t+=1
+    end
+    return adj[:,:,1:t-1]
+end
+
+
+struct WattsStrogatzTemporal <: TemporalNetwork
+    adjacency::Array{Int64,3}
+    number_nodes::Int64
+    number_snapshots::Int64
+    degree::Int64
+    β::Float64
+
+
+    function WattsStrogatzTemporal(number_nodes::Int64, number_snapshots::Int64, degree::Int64, β::Float64)
+        adj=adj_watts_strogatz_temporal(number_nodes, degree, β)
+        new(adj, number_nodes,size(adj,3) , degree, β)
     end
 end
--- experiments_SW/experimentsSW.jl
+++ experiments_SW/experimentsSW.jl
@@ -13,7 +13,7 @@ end
 
 number_nodes = 150
 number_snapshots = 50
-degree =  75
+deg =  75
 
 betas = [0.0001,0.0005,0.001,0.01,0.02,0.07,0.1,0.2,0.5,1.0]
 
@@ -22,7 +22,7 @@ tcc = Vector{Float64}(undef, 10)
 cl = Vector{Float64}(undef, 10)
 cl_graphs = Vector{Float64}(undef, 10)
 for i in 1:10
-    graphs[i] = WattsStrogatz(number_nodes, number_snapshots, degree , i/10)
+    graphs[i] = WattsStrogatz(number_nodes, number_snapshots, deg , i/10)
 end
 
 for i in 1:10
@@ -31,4 +31,22 @@ for i in 1:10
     #cl_graphs[i] = clusteringCoeffGraphs(graphs[i])/clusteringCoeffGraphs(graph_β)
 end
 
-plot([cl, tcc],labels=["temporal clustering " "temporal correlation coefficient"],title="number_snapshots=$(number_snapshots)",xlabel="β",ylabel="value",legend=:topright)
+p1=plot((1:10)./10,[cl, tcc],labels=["temporal clustering " "temporal correlation coefficient"],title="number_snapshots=$(number_snapshots)",xlabel="β",ylabel="value",legend=:topright)
+
+#Temporal
+
+graphs =  Vector{WattsStrogatzTemporal}(undef, 10)
+tcc = Vector{Float64}(undef, 10)
+cl = Vector{Float64}(undef, 10)
+cl_graphs = Vector{Float64}(undef, 10)
+for i in 1:10
+    graphs[i] = WattsStrogatzTemporal(number_nodes, number_snapshots, deg , i/10)
+end
+
+for i in 1:10
+    cl[i] = clusteringCoeff(graphs[i])
+    tcc[i] = temporalCorrelationCoefficient(graphs[i])
+    #cl_graphs[i] = clusteringCoeffGraphs(graphs[i])/clusteringCoeffGraphs(graph_β)
+end
+
+p2=plot((1:10)./10,[cl, tcc],labels=["temporal clustering " "temporal correlation coefficient"],title="number_snapshots=$(number_snapshots)",xlabel="β",ylabel="value",legend=:bottomright)
