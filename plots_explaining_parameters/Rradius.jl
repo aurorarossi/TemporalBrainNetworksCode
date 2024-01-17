51c82690c011564b5131ef8743b89611cabc99c3
aurorarossi <aurora98rossi@gmail.com>
2024-01-17 14:03:21 +0100

    factoring

---
+++ plots_explaining_parameters/Rradius.jl
@@ -0,0 +1,10 @@
+using  Plots, JLD2, LaTeXStrings
+
+αrange = collect(0.5:0.025:1.2)
+Rrange = collect(0.5:0.5:14)
+
+@load "../hyperbolicbrains/optimization/hyperbolic_300_alpha_$(round(Int,αrange[end]*100))_R_05_05_14_v_01_01_09_correct.jld2" avHY clHY plHY tccHY
+
+p = plot(Rrange, avHY[end, :], grid=false, xticks=([2.5, 5.0, 7.5, 10.0, 12.5], [L"2.5", L"5.0", L"7.5", L"10.0", L"12.5"]), yticks=([0, 50, 100, 150, 200], [L"0", L"50", L"100", L"150", L"200"]), color=:black, lw=1.5, legend=false, xlabel=L"R", ylabel=L"\mathrm{Average\,\,  degree}", dpi=1200)
+
+savefig(p, "images/Rradius.png")
